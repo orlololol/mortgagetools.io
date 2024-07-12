1febe7cea6dde85b5bc3a63b2cc8d743b9f9a8db
Orlando Qiu <orlandoqiu@gmail.com>
2024-07-12 13:30:20 -0400

    updated middleware

--- my-nextjs-app/middleware.ts
+++ my-nextjs-app/middleware.ts
@@ -1,9 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
   "/api/webhooks/clerk(.*)",
+  "/api/webhooks/stripe(.*)",
 ]);
 
 export default clerkMiddleware((auth, request) => {
